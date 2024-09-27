98e4c6cc6e90469915867bfb965bedf18689cf0a
DiegoHernandez <diego0930@gmail.com>
2024-09-26 20:02:32 -0500

    c6

--- src/main/java/com/tesla/crud/person/Person.java
+++ src/main/java/com/tesla/crud/person/Person.java
@@ -1,6 +1,24 @@
 package com.tesla.crud.person;
+import jakarta.persistence.*;
+//Esta anotación le indica a JPA que la clase Persona es una entidad.
+// Esto significa que será mapeada a una tabla en la base de datos.
+// Cada instancia de esta clase representará una fila en la tabla.
+@Entity
+//Define el nombre de la tabla en la base de datos a la que
+// esta entidad está mapeada. En este caso, la tabla se llamará personas.
+// Si no colocas esta anotación, JPA por defecto tomará el nombre de la clase
+// como nombre de la tabla.
+@Table(name = "personas")
 
 public class Person {
+    //Esta anotación se coloca antes del campo id para indicar que este atributo será la clave primaria de la tabla.
+    // Cada registro en la tabla debe tener un identificador único y este campo será ese identificador.
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY):
+    // Indica que el valor del campo id se generará automáticamente por la base de datos.
+    // En este caso, estamos usando la estrategia IDENTITY, que le dice a la base de datos
+    // que maneje la generación del valor del id (por ejemplo, autoincrementando en MySQL).
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String lastName;
