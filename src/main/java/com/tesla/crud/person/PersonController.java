0ecb9cbaa78c569804cd9c9a4945ada433d3536c
DiegoHernandez <diego0930@gmail.com>
2024-09-26 21:34:44 -0500

    c8

--- src/main/java/com/tesla/crud/person/PersonController.java
+++ src/main/java/com/tesla/crud/person/PersonController.java
@@ -1,22 +1,62 @@
 package com.tesla.crud.person;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/person")
 public class PersonController {
+
     private final PersonService personService;
     @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
-    @GetMapping(path="/getNames")
-    public List<Person> getNames() {
-        return personService.getNames();
+    @GetMapping
+    public List<Person> findAll() {
+        return personService.findAll();
     }
+    @GetMapping("/{id}")
+    public ResponseEntity<Person> findById(@PathVariable Long id) {
+        Person persona = personService.findById(id);
+        if (persona != null) {
+            return new ResponseEntity<>(persona, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public Person create(@RequestBody Person persona) {
+        return personService.save(persona);
+    }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<Person> update(@PathVariable Long id, @RequestBody Person persona) {
+      Person existingPersona = personService.findById(id);
+      if (existingPersona != null) {
+          existingPersona.setName(persona.getName());
+          existingPersona.setLastName(persona.getLastName());
+          existingPersona.setAge(persona.getAge());
+          existingPersona.setDni(persona.getDni());
+          existingPersona.setGender(persona.getGender());
+          existingPersona.setEmail(persona.getEmail());
+          return new ResponseEntity<>(personService.save(existingPersona), HttpStatus.OK);
+      }
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+  }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Person persona = personService.findById(id);
+        if (persona != null) {
+            personService.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
 }
--- src/main/java/com/tesla/crud/person/PersonService.java
+++ src/main/java/com/tesla/crud/person/PersonService.java
@@ -1,22 +1,28 @@
 package com.tesla.crud.person;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PersonService {
-    public List<Person> getNames() {
-        return List.of(
-                new Person(
-                        123L,
-                        "Diego",
-                        "Hernandez",
-                        36,
-                        101010,
-                        "M",
-                        "diego123@gmail.com"
-                )
-        );
+    @Autowired
+    private PersonaRepository personaRepository;
+
+    public List<Person> findAll() {
+        return personaRepository.findAll();
+    }
+
+    public Person save(Person persona) {
+        return personaRepository.save(persona);
+    }
+
+    public Person findById(Long id) {
+        return personaRepository.findById(id).orElse(null);
+    }
+
+    public void deleteById(Long id) {
+        personaRepository.deleteById(id);
     }
 }
